69990cfde8f8d362b83ee33d4b39f362ff936122
oskaripsen <oskaripsen@yahoo.dk>
2025-02-14 10:49:10 +0100

    Fix Heroku deployment structure

--- game_frontend/lib/config.dart
+++ game_frontend/lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String apiUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://localhost:5000',
+    defaultValue: 'https://energy-game-api-6a18fc829f3d.herokuapp.com',
   );
 }
